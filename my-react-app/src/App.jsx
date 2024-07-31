b8d50a0a52d32285f08fa84a99e0f6335ad5c482
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-07-31 19:29:00 +0200

    learning how to create routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,18 @@
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Main from './components/Main/Main.jsx';
+import Home from './components/Pages/Home.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return(
     <>
-      <Header isLoggedIn={true} name="Amir"/>
-      <Main/>
-      <br />
-      <Footer/>
+      <Header></Header>
+      <Routes>
+        <Route exact path='/' element={<Main/>}/>
+      </Routes>
+      
+      <Footer></Footer>
     </>
   )
 }
